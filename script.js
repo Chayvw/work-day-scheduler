18756136441493530f757379edb2f754d940c789
Chay Williams <Chayvw@gmail.com>
2020-08-18 14:28:04 -0400

    added on click function for save btn

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-$(document).ready(function(){
-    $(".saveBtn").on("clikc", function(){
-        var value = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+$(document).ready(function() {
+    // listen for save button clicks
+    $(".saveBtn").on("click", function() {
+      // get nearby values
+      var value = $(this).siblings(".description").val();
+      var time = $(this).parent().attr("id");
+  
+      // save in localStorage
+      localStorage.setItem(time, value);
+    });
 
-        localStorage.setItem(time, value);
     
 
-}
+    
