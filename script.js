7bc4ec3e79b14f5c3a6a01d8591c59ffbb94cb60
Chay Williams <Chayvw@gmail.com>
2020-08-18 14:36:23 -0400

    set up function to loop over time blocks

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
 $(document).ready(function() {
-    // listen for save button clicks
     $(".saveBtn").on("click", function() {
-      // get nearby values
-      var value = $(this).siblings(".description").val();
-      var time = $(this).parent().attr("id");
+      const value = $(this).siblings(".description").val();
+      const time = $(this).parent().attr("id");
   
-      // save in localStorage
+    // add time and value to local storrage
       localStorage.setItem(time, value);
     });
 
     
-
+    function hourUpdater() {
+        // get current number of hours
+        const currentTime = moment().hours();
     
+        
+        $(".time-block").each(function() {
+          const blockHour = parseInt($(this).attr("id").split("-")[1]);
+    
+         
+          if (blockHour < currentTime) {
+            $(this).addClass("past");
+          } 
+          else if (blockHour === currentTime) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+          } 
+          else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+          }
+        });
+      }
+      hourUpdater();
+    
+
+      $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+  // display current day on page
+  $("#currentDay").text(moment().format("dddd, MMMM Do"));
+});
